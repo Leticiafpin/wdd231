d84df375fa74f1a258624356f043d8fe4533108d
Leticia Figueiredo <pflele1@gmail.com>
2025-03-13 11:00:44 +0000

    correções gerais

--- scripts/date.js
+++ scripts/date.js
@@ -1,3 +1,5 @@
+// Atualiza o ano
 document.getElementById("year").textContent = new Date().getFullYear();
+// Atualiza a última modificação
 document.getElementById("lastModified").textContent = `Last Modified: ${document.lastModified}`;
 
