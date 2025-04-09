c869345a9cb6fe864a3b4ea91d0ae4e4aea1e9af
Leticia Figueiredo <pflele1@gmail.com>
2025-04-09 18:24:12 +0100

    base of site

--- final project/script.js
+++ final project/script.js
@@ -1,7 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log("Travel Guide Loaded!");
 
-    // Example of LocalStorage usage
+    // Modal functionality
+    const modal = document.querySelector(".modal");
+    const openModal = document.querySelector(".open-modal");
+    const closeModal = document.querySelector(".close-modal");
+
+    openModal.addEventListener("click", () => {
+        modal.style.display = "block";
+    });
+
+    closeModal.addEventListener("click", () => {
+        modal.style.display = "none";
+    });
+
+    // LocalStorage Example
     const visitCount = localStorage.getItem("visitCount") || 0;
     localStorage.setItem("visitCount", Number(visitCount) + 1);
     console.log(`You have visited this site ${localStorage.getItem("visitCount")} times.`);
